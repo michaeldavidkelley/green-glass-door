7aee8cd23f03e5ec51ec9d2415dd009cff4d512e
Michael Kelley <mkelley@digitalocean.com>
2022-08-26 21:08:50 -0400

    The Green Glass Door

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 || os.Args[1] == "" {
+		fmt.Println("Please enter a word.")
+		return
+	}
+
+	prevR := '\n'
+	for _, r := range os.Args[1] {
+		if prevR == r {
+			fmt.Println("Fits")
+			return
+		}
+		prevR = r
+	}
+
+	fmt.Println("Does not fit")
+}
